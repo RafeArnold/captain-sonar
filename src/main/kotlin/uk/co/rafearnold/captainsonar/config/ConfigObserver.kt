8dffe1ee2622185fb9a900dc81a7a0015504cbc4
RafeArnold <rafe.arnold@gmail.com>
2022-04-04 15:19:26 +0100

    Add ConfigObserver.

---
+++ src/main/kotlin/uk/co/rafearnold/captainsonar/config/ConfigObserver.kt
@@ -0,0 +1,19 @@
+package uk.co.rafearnold.captainsonar.config
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import uk.co.rafearnold.captainsonar.common.Register
+import java.util.concurrent.CompletableFuture
+import javax.inject.Inject
+
+class ConfigObserver @Inject constructor(
+    private val config: ObservableMap<String, String>
+) : Register {
+
+    override fun register(): CompletableFuture<Void> =
+        CompletableFuture.runAsync { config.addListener(".*") { log.info(it.toString()) } }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(ConfigObserver::class.java)
+    }
+}
